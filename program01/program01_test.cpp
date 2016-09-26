4052dee96bb1dcc5fb670848cc173e4024419d79
Josh Vega <jsvcycling.live@gmail.com>
2016-09-25 17:02:47 -0700

    Trying to implement line generation.

--- program01/program01_test.cpp
+++ program01/program01_test.cpp
@@ -5,6 +5,48 @@
 
 #define DEBUG
 
+void print_line(std::list<char *> tokens, int line_len) {
+	std::list<char *> print_list;
+	int slots_left = line_len;
+	int space_slots = 0;
+
+	while (tokens.size() > 0) {
+#ifdef DEBUG
+		printf("space_slots = %i\tslots_left = %i\n", space_slots, slots_left);
+#endif
+		if ((space_slots * 3) < slots_left) {
+			if (strlen(tokens.front()) < (slots_left - space_slots)) {
+#ifdef DEBUG
+				printf("Next token: %s\tstrlen: %i\n", tokens.front(), (int)strlen(tokens.front()));
+#endif
+				slots_left -= strlen(tokens.front());
+				space_slots += 1;
+
+				print_list.push_back(tokens.front());
+				tokens.pop_front();
+			} else if (strlen(tokens.front()) == (slots_left - space_slots)) {
+#ifdef DEBUG
+				printf("Final token: %s\n", tokens.front());
+#endif
+				slots_left -= strlen(tokens.front());
+
+				print_list.push_back(tokens.front());
+				tokens.pop_front();
+				break;
+			} else {
+				// TODO: split the word.
+
+				slots_left -= strlen(tokens.front());
+				print_list.push_back(tokens.front());
+				tokens.pop_front();
+				break;
+			}
+		} else {
+			break;
+		}
+	}
+}
+
 int main(int argc, char **argv) {
 	if (argc < 2) exit(1);
 
@@ -22,7 +64,7 @@ int main(int argc, char **argv) {
 #ifdef DEBUG
 		// printf("%s\t%i", buf, (int)strlen(buf));
 #endif
-		if (strlen(buf) == 1) {
+		if (strlen(buf) <= 1) {
 #ifdef DEBUG
 			printf("Empty line...\n");
 #endif
@@ -66,23 +108,32 @@ int main(int argc, char **argv) {
 				break;
 			} else {
 #ifdef DEBUG
-				printf("%s\n", token);
+				printf("%s\t%i\n", token, (int)strlen(token));
 #endif
 				token_list.push_back(token);
+				printf("%s\n", token_list.back());
 				token = strtok(NULL, " \t\n");
 			}
 		}
 
 		if (generate) {
-			// TODO
+#ifdef DEBUG
 			printf("Let's generate a paragraph...\n");
+#endif
+			// while (token_list.size() > 0) {
+				// print_line(token_list, line_len);
+			// }
 			token_list.clear();
 		}
 	}
 
 	if (token_list.size() > 0) {
-		// TODO
-		printf("Let's generate a paragraph...\n");
+#ifdef DEBUG
+		printf("Let's generate our final paragraph...\n");
+#endif
+		// while (token_list.size() > 0) {
+			// print_line(token_list, line_len);
+		// }
 		token_list.clear();
 	}
 
