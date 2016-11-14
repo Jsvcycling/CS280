c3b9eb8bd905cc25f15f770dfcaf3abc31e31a0d
Josh Vega <jsvcycling@live.com>
2016-11-13 20:40:11 -0500

    Made some progress (I think) on project 3.

--- program03/project3.cpp
+++ program03/project3.cpp
@@ -4,11 +4,6 @@
 #include <fstream>
 using namespace std;
 
-/////////
-//// this class can be used to represent the parse result
-//// the various different items that the parser might recognize can be
-//// subclasses of ParseTree
-
 class ParseTree {
 private:
 	ParseTree *leftChild;
@@ -17,30 +12,51 @@ private:
 	int	whichLine;
 
 public:
-	ParseTree(ParseTree *left = 0, ParseTree *right = 0) : leftChild(left),rightChild(right) {
+	ParseTree(ParseTree *left = 0, ParseTree *right = 0) : leftChild(left), rightChild(right) {
 		whichLine = linenum;
 	}
 
 	int onWhichLine() { return whichLine; }
 };
 
-//// for example, an integer...
-class Integer : public ParseTree {
+// An identifier
+class Id : public ParseTree {
 private:
-	Token	iTok;
+	Token tok;
 
 public:
-	Integer(const Token& iTok) : ParseTree(), iTok(iTok) {}
+	Id(const Token &t) : ParseTree(), tok(t) { }
 
-	int	getInteger() { return stoi( iTok.getLexeme() ); }
+	string getId() { return t.getLexeme(); }
+};
+
+// An integer
+class Integer : public ParseTree {
+private:
+	Token tok;
+
+public:
+	Integer(const Token &t) : ParseTree(), tok(t) { }
+
+	int getInteger() { return stoi(t.getLexeme()); }
+};
+
+// A string
+class Str : public ParseTree {
+private:
+	Token tok;
+
+public:
+	Str(const Token &t, ParseTree *left = 0, ParseTree *right = 0) : ParseTree(left, right), tok(t) { }
+
+	string getString() { return t.getLexeme() };
 };
 
 int linenum = 0;
 int globalErrorCount = 0;
 
 /// error handler
-void error(string msg)
-{
+void error(string msg) {
 	cout << linenum << ": " << msg << endl;
 	++globalErrorCount;
 }
@@ -54,74 +70,117 @@ ParseTree *Term(istream *in);
 ParseTree *Primary(istream *in);
 ParseTree *String(istream *in);
 
-
-ParseTree *Program(istream *in)
-{
+/* Program -> <StmtList> */
+ParseTree *Program(istream *in) {
 	ParseTree *result = StmtList(in);
 
 	// make sure there are no more tokens...
-	if( getToken(in).getTok() != DONE )
+	if (getToken(in).getTok() != DONE) {
 		return 0;
+	}
 
 	return result;
 }
 
+/* StmtList -> <Stmt> | <Stmt> <StmtList> */
+ParseTree *StmtList(istream *in) {
+	ParseTree *left = Stmt(in);
 
-ParseTree *StmtList(istream *in)
-{
-	return 0;
+	// TODO: Somehow we have to do right as well...
+	ParseTree *right = 0;
+
+	return new ParseTree(left, right);
 }
 
-
-ParseTree *Stmt(istream *in)
-{
+/* Stmt -> PRINT <Expr> SC | SET ID <Expr> SC */
+ParseTree *Stmt(istream *in) {
 	Token t;
 
 	t = getToken(in);
 
-	if( t.getTok() == PRINT ) {
-		// process PRINT
-	}
-	else if( t.getTok() == SET ) {
-		// process SET
-	}
+	if (t.getTok() == PRINT) {
+		ParseTree *left = Expr(in);
 
-	else
+		if (ex == 0) {
+			return 0;
+		}
+
+		t = getToken(in);
+
+		if (t.getTok() != SC) {
+			error("expected semicolon");
+			return 0;
+		}
+
+		return new ParseTree(left);
+	} else if (t.getTok() == SET) {
+		t = getToken(in);
+
+		if (t.getTok() != ID) {
+			error("expected id");
+			return 0;
+		}
+
+		ParseTree *left = Id(t);
+		ParseTree *right = Expr(in);
+
+		if (ex == 0) {
+			return 0;
+		}
+
+		t = getToken(in);
+
+		if (t.getTok() != SC) {
+			error("expected semicolon");
+			return 0;
+		}
+
+		return new ParseTree(left, right);
+	} else {
 		error("Invalid statement");
+	}
 
 	return 0;
 }
 
-
-ParseTree *Expr(istream *in)
-{
+/* Expr -> <Expr> PLUS <Term> | <Term> */
+ParseTree *Expr(istream *in) {
 	return 0;
 }
 
-
-ParseTree *Term(istream *in)
-{
+/* Term -> <Term> STAR <Primary> | <Primary> */
+ParseTree *Term(istream *in) {
 	return 0;
 }
 
-
-ParseTree *Primary(istream *in)
-{
+/* Primary -> ID | <String> | INT | LPAREN <Expr> RPAREN */
+ParseTree *Primary(istream *in) {
 	Token t = getToken(in);
 
-	if( t.getTok() == ID ) {
-	}
-	else if( t.getTok() == INT ) {
+	if (t.getTok() == ID) {
+		return new Id(t);
+	} else if (t.getTok() == INT) {
 		return new Integer(t);
-	}
-	else if( t.getTok() == STR ) {
-	}
-	else if( t.getTok() == LPAREN ) {
-		ParseTree *ex = Expr(in);
-		if( ex == 0 )
+	} else if (t.getTok() == STR) {
+		// We can't do this though since the token is already read...
+		return String(in);
+	} else if (t.getTok() == LPAREN) {
+		t = getTok(in);
+
+		if (t.getTok() != ID) {
+			error("expected id");
 			return 0;
+		}
+		
+		ParseTree *ex = Expr(in);
+		
+		if (ex == 0) {
+			return 0;
+		}
+		
 		t = getToken(in);
-		if( t.getTok() != RPAREN ) {
+		
+		if (t.getTok() != RPAREN) {
 			error("expected right parens");
 			return 0;
 		}
@@ -132,19 +191,39 @@ ParseTree *Primary(istream *in)
 	return 0;
 }
 
+/* String -> STR | STR LEFTSQ <Expr> RIGHTSQ | STR LEFTSQ <Expr> SC <Expr> RIGHTSQ */
+ParseTree *String(istream *in) {
+	Token t = getToken(in);
 
-ParseTree *String(istream *in)
-{
-	return 0;
+	if (t.getTok() == STR) {
+		// TODO: we have to peek forward and check for a left square bracket...
+	} else {
+		error("expected string");
+		return 0;
+	}
 }
 
+int main(int argc, char **argv) {
+	istream *in = &cin;
+	fstream infile;
 
+	for (int i = 1; i < argc; i++) {
+		if (in != &cin) {
+			// TODO
+		}
 
-main()
-{
-	ParseTree *prog = Program(&cin);
+		infile.open(argv[i]);
 
-	if( prog == 0 || globalErrorCount != 0 ) {
+		if (!infile.is_open()) {
+			// TODO
+		}
+
+		in = &infile;
+	}
+	
+	ParseTree *prog = Program(in);
+
+	if (prog == 0 || globalErrorCount != 0) {
 		cout << "Parse failed, exiting" << endl;
 		return 0;
 	}
